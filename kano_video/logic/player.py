fd39e91f594c259ef7c1e3a4a79b86ec678cb9d4
Tom Bettany <tom.bettany@gmail.com>
2014-06-10 12:16:31 +0100

    Centres the windowed player. (KanoComputing/peldins#931)

--- kano_video/logic/player.py
+++ kano_video/logic/player.py
@@ -5,6 +5,7 @@
 #
 
 import sys
+from gi.repository import Gdk
 
 from kano.utils import write_file_contents, is_installed, run_bg, is_running, run_cmd
 from kano.logging import logger
@@ -29,7 +30,12 @@ def play_video(_button=None, video_url=None, localfile=None, fullscreen=False, w
         link = localfile
 
     logger.info('Launching player...')
+
+    width = 480
+    height = 270
+
     if omxplayer_present:
+        x1, y1, x2, y2 = get_centred_coords(width=width, height=height)
         HDMI = False
         try:
             from kano_settings.config_file import get_setting
@@ -45,8 +51,11 @@ def play_video(_button=None, video_url=None, localfile=None, fullscreen=False, w
         if fullscreen:
             player_cmd = 'lxterminal -e "omxplayer {hdmi_str} -b \\"{link}\\""'.format(link=link, hdmi_str=hdmi_str)
         else:
-            file_str = 'kano-window-tool -dno -t omxplayer -x 100 -y 100 -w 300 -h 180\n'
-            file_str += 'omxplayer {hdmi_str} --win "100 100 420 280" "{link}"\n'.format(link=link, hdmi_str=hdmi_str)
+            file_str = 'kano-window-tool -dno -t ' \
+                'omxplayer -x {x} -y {y} -w {width} -h {height}\n'.format(x=x1, y=y1, width=width, height=height)
+            file_str += 'omxplayer {hdmi_str} ' \
+                '--win "{x1} {y1} {x2} {y2}" ' \
+                '"{link}"\n'.format(link=link, hdmi_str=hdmi_str, x1=x1, y1=y1, x2=x2, y2=y2)
             file_path = '/tmp/omxplayer.sh'
             write_file_contents(file_path, file_str)
             player_cmd = 'lxterminal -t omxplayer -e "bash {}"'.format(file_path)
@@ -54,7 +63,7 @@ def play_video(_button=None, video_url=None, localfile=None, fullscreen=False, w
         if fullscreen:
             player_cmd = 'vlc -f --no-video-title-show "{link}"'.format(link=link)
         else:
-            player_cmd = 'vlc --width 700 --no-video-title-show "{link}"'.format(link=link)
+            player_cmd = 'vlc --width {width} --no-video-title-show "{link}"'.format(link=link, width=width)
 
     if not fullscreen and is_running('kdesk'):
         player_cmd = '/usr/bin/kdesk -b \'{}\''.format(player_cmd)
@@ -63,6 +72,22 @@ def play_video(_button=None, video_url=None, localfile=None, fullscreen=False, w
         run_cmd(player_cmd)
     else:
         run_bg(player_cmd)
+
+
+def get_centred_coords(width, height):
+    taskbar_height = 44
+
+    monitor = {
+        'width': Gdk.Screen.width(),
+        'height': Gdk.Screen.height(),
+    }
+
+    x1 = (monitor['width'] - width) / 2
+    x2 = x1 + width
+    y1 = ((monitor['height'] - taskbar_height) - height) / 2
+    y2 = y1 + height
+
+    return x1, y1, x2, y2
 
 
 def stop_videos(_button=None):
