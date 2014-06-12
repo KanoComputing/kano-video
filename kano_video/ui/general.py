a8bff7eb8e4c1300c097dfa097919382728686d2
Radu Jipa <radu.t.jipa@gmail.com>
2014-06-12 15:04:52 +0100

    Update general.py

--- kano_video/ui/general.py
+++ kano_video/ui/general.py
@@ -1,3 +1,11 @@
+# general.py
+# 
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
+# 
+
+
 from gi.repository import Gtk, Gdk
 
 from kano.gtk3.cursor import attach_cursor_events
