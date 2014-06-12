7bd5f77ada1d7ed8bc4f55e302317376e74a7d74
Radu Jipa <radu.t.jipa@gmail.com>
2014-06-12 15:07:35 +0100

    Update popup.py

--- kano_video/ui/popup.py
+++ kano_video/ui/popup.py
@@ -1,3 +1,11 @@
+# popup.py
+# 
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
+# 
+
+
 import os
 from gi.repository import Gtk
 
