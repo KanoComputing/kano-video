ed08c921de7d7aa4c881c71c438ceb777abeb824
Radu Jipa <radu.t.jipa@gmail.com>
2014-06-12 15:03:49 +0100

    Update bar.py

--- kano_video/ui/bar.py
+++ kano_video/ui/bar.py
@@ -1,3 +1,11 @@
+# bar.py
+# 
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
+# 
+
+
 from gi.repository import Gtk, Gdk
 import os
 from shutil import rmtree
