2721a3d798111fe0ffd762f0470ff044fe8f6007
Tom Bettany <tom.bettany@gmail.com>
2014-06-16 16:32:14 +0100

    Fixes some small linting issues.

--- kano_video/ui/header.py
+++ kano_video/ui/header.py
@@ -1,9 +1,9 @@
 # header.py
-# 
+#
 # Copyright (C) 2014 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
 #
-# 
+#
 
 
 from gi.repository import Gtk
