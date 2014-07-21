f394803b514437e0c30d4c7c987f875b9f720cee
Radu Jipa <radu.t.jipa@gmail.com>
2014-07-21 20:22:43 +0100

    Added more omx keys https://github.com/KanoComputing/peldins/issues/1050

--- kano_video/logic/playudev.py
+++ kano_video/logic/playudev.py
@@ -13,10 +13,10 @@ import subprocess
 import threading
 
 
-def wait_for_keys (pomx):
+def wait_for_keys(pomx):
     '''
     Listens for keyboard events from /dev/input
-    translates Q and Space to omxplayer via its stdin.
+    translates ESC, Q, Space, P, -, + to omxplayer via its stdin.
     pomx is a subprocess Popen object.
     '''
 
@@ -35,24 +35,40 @@ def wait_for_keys (pomx):
     while event:
         (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
 
-        if type == 1 and code == 16 and value == 0:
-            # The key "q" has been released, quit omxplayer
+        # other keys you wish to send to omxplayer should be added here
+        # future updates to omxplayer need to be taken into account here
+
+        #print "type {} | code {} | value {}".format(type, code, value)
+
+        if (type == 1 and code == 1 and value == 0) or (type == 1 and code == 16 and value == 0):
+            # The key "esc" or "q" has been released, quit omxplayer
             pomx.stdin.write('q')
             pomx.stdin.flush()
 
             # finish the thread
             break
 
-        if type == 1 and code == 57 and value == 0:
-            # The space key has been released, pause/resume the media
+        elif (type == 1 and code == 25 and value == 0) or (type == 1 and code == 57 and value == 0):
+            # The key "p" or "space" has been released, pause/resume the media
             pomx.stdin.write(' ')
+            pomx.stdin.flush()
+
+        elif type == 1 and code == 12 and value == 0:
+            # The key "-" has been released, decrease the volume
+            pomx.stdin.write('-')
+            pomx.stdin.flush()
+
+        elif type == 1 and code == 13 and value == 0:
+            # The key "+" has been released, increase the volume
+            pomx.stdin.write('+')
             pomx.stdin.flush()
 
         event = in_file.read(EVENT_SIZE)
 
     in_file.close()
 
-def run_player (cmdline):
+
+def run_player(cmdline):
     '''
     Start omxplayer giving us the stdin pipe to speak to him.
     '''
